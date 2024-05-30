0ff7351797c8aed4eef9069b1f41ab53d94ac72d
abaoleon <johnleonardabao@gmail.com>
2024-05-30 16:49:03 +0800

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,67 @@
+function input1(){
+
+    document.getElementById("label40").style.color="green";
+    document.getElementById("label41").style.color="red";
+    document.getElementById("label42").style.color="red";
+}
+function input2(){
+
+    document.getElementById("label2").style.color="red";
+    document.getElementById("label1").style.color="green";
+    document.getElementById("label3").style.color="green";
+    
+}
+function input3(){
+
+    document.getElementById("label4").style.color="red";
+    document.getElementById("label5").style.color="red";
+    document.getElementById("label6").style.color="green";
+}
+
+
+function input4(){
+
+    document.getElementById("label7").style.color="red";
+    document.getElementById("label8").style.color="green";
+    document.getElementById("label9").style.color="red";
+
+}
+function input5(){
+    document.getElementById("label10").style.color="red";
+    document.getElementById("label11").style.color="red";
+    document.getElementById("label12").style.color="green";
+
+}
+function input6(){
+    
+    document.getElementById("label13").style.color="green";
+    document.getElementById("label14").style.color="red";
+    document.getElementById("label15").style.color="red";
+}
+function input7(){
+    
+    document.getElementById("label16").style.color="red";
+    document.getElementById("label17").style.color="red";
+    document.getElementById("label18").style.color="green";
+}
+function input8(){
+    
+    document.getElementById("label19").style.color="green";
+    document.getElementById("label20").style.color="red";
+    document.getElementById("label21").style.color="red";
+}
+function input9(){
+    
+    document.getElementById("label22").style.color="green";
+    document.getElementById("label23").style.color="red";
+    document.getElementById("label24").style.color="red";
+}
+function input10(){
+    
+    document.getElementById("label25").style.color="red";
+    document.getElementById("label26").style.color="green";
+    document.getElementById("label27").style.color="red";
+}
+   
+
+
